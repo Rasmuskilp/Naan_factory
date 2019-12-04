46824f7c020b92862b4501339c2d645c6f6ccbae
Rasmus Kilp <RKilp@spartaglobal.com>
2019-12-04 14:40:16 +0000

    added 2 statements for not dough/Naan

--- factory_tests.py
+++ factory_tests.py
@@ -6,14 +6,19 @@
 # inputs = 'water' and 'flour'
 # outputs = 'dough'
 def make_dough(ing_1,ing_2):
-    pass
+    return 'dough'
 def bake_dough(arg1):
-    pass
+    return 'Naan'
 print("Testing make_dough with 'water' and 'flour'. Expected -->'dough'")
 print(make_dough('water','flour') == 'dough')
 print('got:',make_dough('water','flour'))
+make_dough('concrete','water') == 'Not dough'
+#When I pass in cement wand water, or anything else... I should get : 'not dough'
+
 #Make test for bake_dough
 print("Testing bake_dough, with 'dough'. Expected --> 'Naan'")
 #then with the dough should be able to put it to even and get out Naan
 print(bake_dough('dough') == 'Naan')
 print('got:',bake_dough('dough'))
+#when bake_dough is passed something other than dough, it should output 'not Naan'
+bake_dough('chicken') == 'Not Naan'
