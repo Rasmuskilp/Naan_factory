1d0f1b3fe0d2097c1a2ab4563ad818e6f0431950
Rasmus Kilp <RKilp@spartaglobal.com>
2019-12-04 15:08:38 +0000

    all tests passing

--- factory_tests.py
+++ factory_tests.py
@@ -6,19 +6,34 @@
 # inputs = 'water' and 'flour'
 # outputs = 'dough'
 def make_dough(ing_1,ing_2):
-    return 'dough'
+    if ing_1 != 'water' and ing_2 != 'water':
+        return 'not dough'
+    elif ing_1 != 'flour' and ing_2 != 'flour':
+        return 'not dough'
+    elif 'water' in [ing_1,ing_2] and 'flour' in [ing_1,ing_2]:
+        return 'dough'
+#return 'dough'
 def bake_dough(arg1):
-    return 'Naan'
+    if arg1 == 'dough':
+        return 'Naan'
+    else:
+        return 'not Naan'
 print("Testing make_dough with 'water' and 'flour'. Expected -->'dough'")
 print(make_dough('water','flour') == 'dough')
 print('got:',make_dough('water','flour'))
-make_dough('concrete','water') == 'Not dough'
+print(make_dough('concrete','water') == 'not dough')
+print("Testing make_dough with 'concrete' and 'flour'. Expected -->'not dough'")
+print('got:',make_dough('concrete','flour'))
+print(make_dough('flower','carrot') == 'not dough')
 #When I pass in cement wand water, or anything else... I should get : 'not dough'
-
 #Make test for bake_dough
 print("Testing bake_dough, with 'dough'. Expected --> 'Naan'")
 #then with the dough should be able to put it to even and get out Naan
 print(bake_dough('dough') == 'Naan')
 print('got:',bake_dough('dough'))
 #when bake_dough is passed something other than dough, it should output 'not Naan'
-bake_dough('chicken') == 'Not Naan'
+print(bake_dough('chicken') == 'not Naan')
+print("Testing bake_dough, with 'dough'. Expected --> 'not Naan'")
+print('got:',bake_dough('chicken'))
+print(bake_dough('wood') == 'not Naan')
+
